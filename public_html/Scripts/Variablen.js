1e5735c91899d74bb07d17424e57ecbc53206be7
Lukas <Lukas@DESKTOP-LC43BCA.fritz.box>
2016-10-06 12:57:01 +0200

    Initialielisierung einiger Variablen

--- public_html/Scripts/Variablen.js
+++ public_html/Scripts/Variablen.js
@@ -6,18 +6,32 @@
 var anzahl = 5;
 
 var uiv = {
-    haupt : $("#Haupt"),
-    main : $("#Main"),
-    levels : [anzahl],
-    levinh :  [anzahl],
-    anzeigen :  [anzahl],
-    KnopfA : [anzahl]
-    };
+    haupt: $("#Haupt"),
+    main: $("#Main"),
+    levels: [anzahl],
+    levinh: [anzahl],
+    anzeigen: [anzahl],
+    KnopfA: [anzahl]
+};
 
 var Game = {
-   Sp : {
-       geld : [],
-       preis : [],
-       anzGek : [],    
-   }
+    Sp: {
+        geld: [],
+        preis: [],
+        anzGek: []
+    },
+    basPreis: [],
+    preiserh: [],
+    prosek: []
+};
+
+//Initialiesierung der Variablen
+for (i = 0; i < anzahl; i++) {
+    Game.Sp.geld[i] = Decimal(0)
+    Game.Sp.anzGek[i] = Decimal(0)
+    Game.prosek[i] = Decimal(0)
+    Game.basPreis[i] = Decimal(i ^ 2 * 100)
+    Game.preiserh[i] = Decimal(1+(0.75*i)^1.5)
+    
+
 }
