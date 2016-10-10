632ab592d4097add24555e5410d099b8b361b571
Lukas <Lukas@DESKTOP-LC43BCA.fritz.box>
2016-10-10 14:16:09 +0200

    Rechnen Funktion Hinzugefügt

--- public_html/Scripts/Spiel.js
+++ public_html/Scripts/Spiel.js
@@ -9,14 +9,21 @@ function kauf(nummer) {
         if(Game.Sp.geld[nummer].gt(0)){
             Game.Sp.geld[1] = Game.Sp.geld[1].sub(Game.preis[nummer]);
             Game.Sp.geld[nummer] = Game.Sp.geld[nummer].mul(1.1);
-            
+            rechnen()
             TextAktu()
         }
         else {
             Game.Sp.geld[1] = Game.Sp.geld[1].sub(Game.preis[nummer]);
             Game.Sp.geld[nummer] = Decimal(1);
-            
+            rechnen()
             TextAktu()
         }
     }
+}
+
+function rechnen() {
+    for (i=1; i<=anzah ;i++) {
+        Game.prosek[i] = Game.Sp.geld[i+1].mul(Game.produmul).add(1);
+        Game.preis[i] = Game.basPreis[i].mul(Game.preiserh.pow(Game.Sp.anzGek));
+    };
 }
