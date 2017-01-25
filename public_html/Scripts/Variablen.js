603ee7515c7a69000682aef46015548f0c92efac
Lukas <Lukas@DESKTOP-LC43BCA.fritz.box>
2017-01-25 16:23:54 +0100

    Chaned Upgrade Base Price

--- public_html/Scripts/Variablen.js
+++ public_html/Scripts/Variablen.js
@@ -77,10 +77,10 @@ function initvars() {
     Game.upgradePriceErh[i] = [];
     for (i1 = 0; i1 < upgradeanzahl; i1++) {
         //Upgrade Effekt Formel
-        Game.upgradeEfekt[i1] = Decimal.mul(0.75, Decimal(1.5).pow(i1));
+        Game.upgradeEfekt[i1] = Decimal.mul(0.75, Decimal(1.8).pow(i1));
         Game.Sp.upgradeGek[i][i1] = Decimal(0);
         //Upgrade BasPreis Formel
-        Game.upgradeBasPreis[i][i1] = Decimal.pow(Decimal.mul(i, Decimal.mul(0.2,Decimal.add(i1,1))).add(1), Decimal(150.66).add(i1).mul(0.05).mul(i)).mul(100);
+        Game.upgradeBasPreis[i][i1] = Decimal.pow(Decimal.mul(i, Decimal.add(0.2,Decimal.mul(i1,0.1))).add(1), Decimal(160.66).mul(0.08).mul(i)).mul(100);
         Game.upgradePreis[i][i1] = Game.upgradeBasPreis[i][i1];
         //Upgrade Preis Erhöungs Formel
         Game.upgradePriceErh[i][i1] = Decimal.mul(0.3, i).add(1).pow(1.4+i1*0.3);
