66c27c94a047337f8acc06d914ddeac7fcdfad42
Lukas <Lukas@DESKTOP-LC43BCA.fritz.box>
2017-01-25 15:55:16 +0100

    UI uses real 2D Arays now

--- public_html/Scripts/UI.js
+++ public_html/Scripts/UI.js
@@ -37,13 +37,16 @@ function generateUpgradesUI(){
         uiv.Upgrades.upgr.append(uiv.Upgrades.levinh[u]);
         uiv.Upgrades.produkmulanzeige[u] = $("<p id='produmulanz"+u+"'>Error1</p>");
         uiv.Upgrades.levinh[u].append(uiv.Upgrades.produkmulanzeige[u]);
+        uiv.Upgrades.kaufKnopf[u] = [];
+        uiv.Upgrades.preisanzeige[u] = [];
+        uiv.Upgrades.gekauftanzeige[u] = [];
         for (u2 = 0; u2<upgradeanzahl; u2++){
-            uiv.Upgrades.kaufKnopf[[u,u2]] = $("<button id='upgrkn"+u+"S"+u2+"'>Error2</button>");
-            uiv.Upgrades.levinh[u].append(uiv.Upgrades.kaufKnopf[[[u,u2]]]);
-            uiv.Upgrades.preisanzeige[[u, u2]] = $("<span id='upgrpreisanz"+u+"S"+u2+"'>Error3</span>");
-            uiv.Upgrades.levinh[u].append(uiv.Upgrades.preisanzeige[[[u,u2]]])
-            uiv.Upgrades.gekauftanzeige[[u,u2]] = $("<span id='upgrgeksanz"+u+"S"+u2+"'>Error4</span>");
-            uiv.Upgrades.levinh[u].append(uiv.Upgrades.gekauftanzeige[[[u,u2]]])
+            uiv.Upgrades.kaufKnopf[[u][u2]] = $("<button id='upgrkn"+u+"S"+u2+"'>Error2</button>");
+            uiv.Upgrades.levinh[u].append(uiv.Upgrades.kaufKnopf[[[u][u2]]]);
+            uiv.Upgrades.preisanzeige[[u][u2]] = $("<span id='upgrpreisanz"+u+"S"+u2+"'>Error3</span>");
+            uiv.Upgrades.levinh[u].append(uiv.Upgrades.preisanzeige[[[u][u2]]])
+            uiv.Upgrades.gekauftanzeige[[u][u2]] = $("<span id='upgrgeksanz"+u+"S"+u2+"'>Error4</span>");
+            uiv.Upgrades.levinh[u].append(uiv.Upgrades.gekauftanzeige[[[u][u2]]])
             uiv.Upgrades.levinh[u].append($("<br/>"))
             uiv.Upgrades.levinh[u].append($("<br/>"))
         }
