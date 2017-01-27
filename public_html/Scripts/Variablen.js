66ae955188ce579a4f4d66059f41a52705447edd
Lukas <Lukas@DESKTOP-LC43BCA.fritz.box>
2017-01-27 22:12:44 +0100

    Increased Upgrade Price Increese

--- public_html/Scripts/Variablen.js
+++ public_html/Scripts/Variablen.js
@@ -85,7 +85,7 @@ function initvars() {
         Game.upgradeBasPreis[i][i1] = Decimal.pow(Decimal.mul(i, Decimal.add(0.2,Decimal.mul(i1,0.1))).add(1), Decimal(160.66).mul(0.08).mul(i)).mul(100);
         Game.upgradePreis[i][i1] = Game.upgradeBasPreis[i][i1];
         //Upgrade Preis Erhöungs Formel
-        Game.upgradePriceErh[i][i1] = Decimal.mul(0.3, i).add(1).pow(1.9+i1*0.5);
+        Game.upgradePriceErh[i][i1] = Decimal.mul(0.4, i).add(1).pow(2.8+i1*1.2);
         
     }
    }
