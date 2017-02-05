63aad07aaf48ed2d69014b801bd5b3b10725494e
Lukas <Lukas@DESKTOP-LC43BCA.fritz.box>
2017-02-05 15:52:21 +0100

    Changed Upgrade Button text

--- public_html/Scripts/UI.js
+++ public_html/Scripts/UI.js
@@ -103,7 +103,7 @@ function TextAktu() {
             $("#KnopfA"+i).css("background-color", "grey");
         $("#produmulanz"+i).text("Produktmul: " + Game.produmul[i].toPrecision(3))
         for (i3=0; i3<upgradeanzahl; i3++) {
-            $("#upgrkn"+i+"S"+i3).text("Multipy your Productionmulti by "+Game.upgradeEfekt[i3].add(1).toPrecision(3))
+            $("#upgrkn"+i+"S"+i3).text("Upgrade "+(i3+1)+ " efficiency = " +Game.upgradeEfekt[i3].add(1).toPrecision(3))
             $("#upgrpreisanz"+i+"S"+i3).text("Price: "+ Game.upgradePreis[i][i3].toPrecision(3))
             $("#upgrgeksanz"+i+"S"+i3).text("  You have bought this "+ Game.Sp.upgradeGek[i][i3].toPrecision(3)+"times")
         }
